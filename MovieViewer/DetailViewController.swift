2897203950e8385cae003b81a9d91f2a3a0d1184
Becky Chan <beckychan@Beckys-MacBook-Air.local>
2017-02-07 01:44:03 -0800

    scroll view & content size

--- MovieViewer/DetailViewController.swift
+++ MovieViewer/DetailViewController.swift
@@ -13,6 +13,8 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var infoView: UIView!
     
     
     var movie: NSDictionary!
@@ -21,6 +23,9 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //scroll view & content size
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: infoView.frame.origin.y + infoView.frame.size.height)
         
         //Youtube: 11:10
         let title = movie["title"] as? String
